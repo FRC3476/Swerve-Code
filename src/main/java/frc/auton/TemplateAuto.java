1d0c2b7472d0e10225649eb155e8fbaac840338a
varun7654 <varun7654@outlook.com>
2022-03-18 15:09:52 -0700

    remove the killswitch  & delayedExecutions from template auto + make sure exceptions aren't being hidden by more broad ones
    the killswitch way to kill autos is no longer used
    the delayedExecutions object in template auto is also no longer used

--- src/main/java/frc/auton/TemplateAuto.java
+++ src/main/java/frc/auton/TemplateAuto.java
@@ -2,21 +2,10 @@ package frc.auton;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import frc.auton.guiauto.serialization.command.SendableScript;
-import frc.subsystem.Drive;
 import frc.subsystem.RobotTracker;
 
-import java.util.ArrayList;
-
 public abstract class TemplateAuto implements Runnable {
-    boolean killSwitch = false;
     protected boolean done = false;
-    Drive drive = Drive.getInstance();
-
-    /**
-     * A function that should be executed at a certain time in the autonomous period.
-     */
-    ArrayList<SendableScript> delayedExecutions;
 
     RobotTracker robotTracker = RobotTracker.getInstance();
 
@@ -32,22 +21,11 @@ public abstract class TemplateAuto implements Runnable {
         return RobotTracker.getInstance().getPoseMeters().getRotation();
     }
 
-    synchronized public void killSwitch() {
-        killSwitch = true;
-        delayedExecutions.clear();
-    }
-
-    synchronized public boolean isDead() {
-        return killSwitch;
-    }
-
     synchronized public boolean isFinished() {
         return done;
     }
 
     public synchronized void reset() {
-        this.killSwitch = false;
         this.done = false;
-        delayedExecutions.clear();
     }
 }
